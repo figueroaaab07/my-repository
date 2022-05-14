2cf21112b7fdb7f1b7c46192d3a50aa06012fb6b
Alejandro Luis Figueroa Cevallos <figueroaaab07@gmail.com>
2022-05-13 21:36:28 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,33 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  function createImageStyle() {
-    let css = document.createElement('style');
-    let imgStyle = '.image { width: 500px; height: 500px; border: solid #ccc; border-width: 3px; object-fit: contain; display: inline-block; margin-left: auto; margin-right: auto; }';
-    let figcaptionStyle = ".caption { width: 180px; height: 180px; font-size: 13px; font-weight: bold; text-align: center }"
-    css.appendChild(document.createTextNode(imgStyle));
-    css.appendChild(document.createTextNode(figcaptionStyle));
-    document.getElementsByTagName("head")[0].appendChild(css);
-  }
-  createImageStyle();
-
-  // function createHtmlBase() {
-  //   let divElement = document.createElement('div');
-  //   divElement.className = "wrapper";
-  //   divElement.appendChild(document.querySelector("h1"));
-  //   let h2Element = document.createElement("h2");
-  //   h2Element.innerText = "Take a sample from more than 490,000 works, throughout the world and history, and and let yourself be carried away by the feelings they inspire"
-  //   divElement.appendChild(h2Element);
-  //   let selectorLabel = document.createElement('label');
-  //   selectorLabel.innerText = "Please Select Collection";
-  //   divElement.appendChild(selectorLabel);
-  //   let selectorElement = document.createElement('select');
-  //   selectorElement.className = "selector";
-  //   divElement.appendChild(selectorElement);
-  //   divElement.appendChild(document.querySelector("#object-image-container"));
-  //   document.querySelector("body").appendChild(divElement);
-  
-  // }
-
   function createHtmlBase() {
     let divElement = document.createElement('div');
     divElement.className = "wrapper";
@@ -35,12 +7,16 @@ document.addEventListener("DOMContentLoaded", () => {
     let h2Element = document.createElement("h2");
     h2Element.innerText = "Take a sample from more than 490,000 works, throughout the world and history, and and let yourself be carried away by the feelings they inspire"
     divElement.appendChild(h2Element);
+    let selectorDiv = document.createElement('div');
+    selectorDiv.className = "custom-select";
+    divElement.appendChild(selectorDiv);
     let selectorLabel = document.createElement('label');
-    selectorLabel.innerText = "Please Select Collection";
-    divElement.appendChild(selectorLabel);
+    selectorLabel.innerText = "Please Select Department:  ";
+    selectorLabel.htmlFor = "selector";
+    selectorDiv.appendChild(selectorLabel);
     let selectorElement = document.createElement('select');
-    selectorElement.className = "selector";
-    divElement.appendChild(selectorElement);
+    selectorElement.id = "selector";
+    selectorDiv.appendChild(selectorElement);
     divElement.appendChild(document.querySelector("#object-image-container"));
     document.querySelector("body").appendChild(divElement);
   }
@@ -50,7 +26,7 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch("https://collectionapi.metmuseum.org/public/collection/v1/departments")
     .then(res => res.json())
     .then(departmentsData => departmentsData["departments"].forEach(department => {
-      let selectorElement = document.querySelector(".selector");
+      let selectorElement = document.querySelector('#selector');
       let optionElement = document.createElement('option');
       optionElement.innerText = department["displayName"];
       optionElement.id = department["departmentId"];
@@ -58,12 +34,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }))
   }
   populateSelector();
-
-  // function getDepartments() {
-  //   return  fetch("https://collectionapi.metmuseum.org/public/collection/v1/departments")
-  //   .then(res => res.json())
-  // }
-  // console.log(getDepartments());
 
   function getObjectData(objectID) {
     return  fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectID}`)
@@ -85,7 +55,7 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector("#object-image-container").appendChild(figureElement);
   }
 
-  function renderImagesByDepartment(departmentId) {
+  function renderImagesByDepartment(departmentId = "1") {
     return  fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=${departmentId}`)
             .then(res => res.json())
             .then(objectsByDepartment => objectsByDepartment["objectIDs"].sort(() =>.5- Math.random()).slice(0, 4))
@@ -93,5 +63,19 @@ document.addEventListener("DOMContentLoaded", () => {
             .then(objectsData => objectsData.map(objectData => renderOneObject(objectData)))
   }
 
+  function optionCb(event) {
+    event.preventDefault();
+    document.querySelector("#object-image-container").innerHTML = "";
+    let selection = event.target.selectedIndex + 1;
+    console.log(selection);
+    renderImagesByDepartment(selection.toString());
+  }
+
+  function getValueSelector() {
+    let optionSelected = document.querySelector("#selector");
+    optionSelected.addEventListener("click", optionCb)
+  }
+
   renderImagesByDepartment(departmentId = "1");
+  getValueSelector();
 })
